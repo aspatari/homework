1b30acb543551f97a51d62a9c3eee41dc1af93de
Artur Spatari <artur.spatari@gmail.com>
2023-06-22 12:22:34 +0300

    fix(core) Fix type of service host

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME = "Backend Homework"
 
-    SERVER_HOST: AnyHttpUrl = "0.0.0.0"
+    SERVER_HOST: str = "0.0.0.0"
     SERVER_PORT: int = 8000
     SERVER_RELOAD: bool = True
 
