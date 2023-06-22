ddf1bef4bb483a8165b6107dc109d2ea5b053efa
Artur Spatari <artur.spatari@gmail.com>
2023-06-22 13:32:30 +0300

    feat(core) Connect api router to app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-
+from api import router
 from core import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
-
+app.include_router(router=router, prefix=settings.API_V1_STR)
 
 if __name__ == "__main__":
     import uvicorn
