d6cb90c3bcb9c2683f6cc101a06df0c9332b63ae
Artur Spatari <artur.spatari@gmail.com>
2023-06-22 13:30:52 +0300

    ref(indexer)  Adding some helper function for dry

--- indexer/utils.py
+++ indexer/utils.py
@@ -4,15 +4,23 @@ from hashlib import md5
 from indexer.schemas import IndexedPost
 
 
+def string_to_md5(input_str: str) -> str:
+    return md5(input_str.encode()).hexdigest()
+
+
+def count_words(input_str: str) -> int:
+    return len(input_str.split())
+
+
 def post_to_indexed_post(post: schemas.PostCreate) -> schemas.IndexedPost:
     indexed_post = IndexedPost(
         created_at=datetime.now(),
-        title_md5=md5(post.title.encode()).hexdigest(),
-        text_md5=md5(post.text.encode()).hexdigest(),
+        title_md5=string_to_md5(post.title),
+        text_md5=string_to_md5(post.text),
         title_len_ch=len(post.title),
         text_len_ch=len(post.text),
-        title_len_words=len(post.title.split()),
-        text_len_words=len(post.text.split()),
+        title_len_words=count_words(post.title),
+        text_len_words=count_words(post.text),
         url_hostname=post.url.host,
         url_scheme=post.url.scheme,
         **post.dict()
